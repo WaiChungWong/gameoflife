e53b04f18aad4e0ce8261fe8e235417ae62d5877
WaiChungWong <johnwongwwc@gmail.com>
2019-02-28 21:41:53 +0800

    fix security vulnerability in dependency

---
+++ build/precache-manifest.74307946d15cf68e20366aac3b612098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/gameoflife/build/static/media/gradient.c7a33805.png"
+  },
+  {
+    "revision": "b2cbb7ddc682e960929a",
+    "url": "/gameoflife/build/static/js/runtime~main.b2cbb7dd.js"
+  },
+  {
+    "revision": "a55a85f0d3fe5b2bb812",
+    "url": "/gameoflife/build/static/js/main.a55a85f0.chunk.js"
+  },
+  {
+    "revision": "89aec2a0ad51f24cf516",
+    "url": "/gameoflife/build/static/js/2.89aec2a0.chunk.js"
+  },
+  {
+    "revision": "a55a85f0d3fe5b2bb812",
+    "url": "/gameoflife/build/static/css/main.e2934770.chunk.css"
+  },
+  {
+    "revision": "89aec2a0ad51f24cf516",
+    "url": "/gameoflife/build/static/css/2.051f0171.chunk.css"
+  },
+  {
+    "revision": "b7f77d6683b476ebd627c36139444406",
+    "url": "/gameoflife/build/index.html"
+  }
+];
